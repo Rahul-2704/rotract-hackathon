5985e86ea5239f87e0cd6fd21b62d23f5b89fc39
SahilV14 <sahil.vedpathak@spit.ac.in>
2023-09-16 22:43:22 +0530

    basic temporary form for manufacturer dashboard (corrected)

--- frontend/app/dashboard/Producer/page.jsx
+++ frontend/app/dashboard/Producer/page.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import Header from "@/app/dashboard/Distributor/header";
+"use client"
+import React, {useState} from 'react';
 
 
 export default function Manufacturer() {
+
+    const [batchID, setBatchID] = useState();
+    const [medName, setMedName] = useState('');
+    const [sealOn, setSealOn] = useState(true);
+    const [count, setCount] = useState();
+
     return(
         <div className='dashboard'>
-            <Header />
-            <div id='content'>
-                <form action="frontend/app/dashboard/Distributor" id='prod-form'>
-                    <input type="number" aria-label='Batch ID'/>
-                    <input type="text" aria-label='Medicine Name'/>
-                    <input type="radio" aria-label='Seal on'/>
-                    <input type="number" aria-label='Count'/>
+            <div id='content' >
+                <form action={'/'} id='prod-form'>
+                    <input type="number" placeholder='Batch ID' onChange={(e)=>setBatchID(e.target.value)} />
+                    <input type="text" placeholder='Medicine Name' onChange={(e)=>setMedName(e.target.value)}/>
+                    <input type="checkbox" placeholder='Seal on'onChange={(e)=> setSealOn(e.target.value)}/>
+                    <label htmlFor='seal'>is sealed</label>
+                    <input type="number" placeholder='Count' id='seal' onChange={(e => setCount(e.target.value))}/>
                     <button type='submit'> SUBMIT</button>
                 </form>
             </div>
