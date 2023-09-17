580d8555f5b478de6bc0cad9f7c2939d9d2f9c5e
Rahul-2704 <rahulruke35@gmail.com>
2023-09-17 08:47:26 +0530

    Home and admin

--- frontend/app/dashboard/distributor/page.jsx
+++ frontend/app/dashboard/distributor/page.jsx
@@ -1,91 +1,102 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 function Distributor() {
+  const handleVerification = () => {
+    alert("Verification process initiated");
+  };
   return (
     <div>
       <section className="mx-auto w-full max-w-7xl px-4 py-4">
-  <div className="flex flex-col space-y-4  md:flex-row md:items-center md:justify-between md:space-y-0">
-    <div>
-      <h2 className="text-lg font-semibold">Medicine Batch</h2>
-      <p className="mt-1 text-sm text-gray-700">
-        This is a list of all batches.
-      </p>
-    </div>
-  </div>
-  <div className="mt-6 flex flex-col">
-    <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-        <div className="overflow-hidden border border-gray-200 md:rounded-lg">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th
-                scope="col"
-                className="px-4 py-3.5 text-left text-sm font-normal text-gray-700"
-                >No.</th>
-                <th
-                  scope="col"
-                  className="px-4 py-3.5 text-left text-sm font-normal text-gray-700"
-                >
-                  <span>Batch Id</span>
-                </th>
-                <th
-                  scope="col"
-                  className="px-12 py-3.5 text-left text-sm font-normal text-gray-700"
-                >
-                  Medicine Name
-                </th>
-                <th
-                  scope="col"
-                  className="px-4 py-3.5 text-left text-sm font-normal text-gray-700"
-                >
-                  Status
-                </th>
-                <th
-                  scope="col"
-                  className="px-4 py-3.5 text-left text-sm font-normal text-gray-700"
-                >
-                  Count
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200 bg-white">
-              <tr>
-                <td className="whitespace-nowrap px-4 py-4">1.</td>
-                <td className="whitespace-nowrap px-4 py-4">
-                  <div className="flex items-center">
-                    <div>
-                      <div className="text-sm font-medium text-gray-900">
-                        bferugbtugtbug
-                      </div>
-                      <div className="text-sm text-gray-700">john@devui.com</div>
-                    </div>
-                  </div>
-                </td>
-                <td className="whitespace-nowrap px-12 py-4">
-                  <div className="text-sm text-gray-900 "></div>
-                  <div className="text-sm text-gray-700">Engineering</div>
-                </td>
-                <td className="whitespace-nowrap px-4 py-4">
-                  <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                    Active
-                  </span>
-                </td>
-                <td className="whitespace-nowrap px-4 py-4 text-sm text-gray-700">
-                  20
-                </td>
-              </tr>
-              
-            </tbody>
-          </table>
+        <div className="flex flex-col space-y-4  md:flex-row md:items-center md:justify-between md:space-y-0">
+          <div>
+            <h2 className="text-lg font-semibold">Medicine Batch</h2>
+            <p className="mt-1 text-sm text-gray-700">
+              This is a list of all batches.
+            </p>
+          </div>
         </div>
-      </div>
+        <div className="mt-6 flex flex-col">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-left text-sm font-normal text-gray-700"
+                      >
+                        No.
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-left text-sm font-normal text-gray-700"
+                      >
+                        <span>Batch Id</span>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-12 py-3.5 text-left text-sm font-normal text-gray-700"
+                      >
+                        Medicine Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-left text-sm font-normal text-gray-700"
+                      >
+                        Status
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-left text-sm font-normal text-gray-700"
+                      >
+                        Count
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    <tr>
+                      <td className="whitespace-nowrap px-4 py-4">1.</td>
+                      <td className="whitespace-nowrap px-4 py-4">
+                        <div className="flex items-center">
+                          <div>
+                            <div className="text-sm font-medium text-gray-900">
+                              bferugbtugtbug
+                            </div>
+                            <div className="text-sm text-gray-700">
+                              john@devui.com
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap px-12 py-4">
+                        <div className="text-sm text-gray-900 "></div>
+                        <div className="text-sm text-gray-700">Engineering</div>
+                      </td>
+                      <td className="whitespace-nowrap px-4 py-4">
+                        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                          Active
+                        </span>
+                      </td>
+                      <td className="whitespace-nowrap px-4 py-4 text-sm text-gray-700">
+                        <button
+                          onClick={handleVerification}
+                          className="px-2 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+                        >
+                          Verify
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  </div>
-</section>
-</div>
-  )
+  );
 }
 
-export default Distributor
+export default Distributor;
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,14 +1,67 @@
 
 import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
   return (
-      <div>
-      <Image
-      src="/image.png"
-      width={500}
-      height={500}
-      alt="Picture of the author"
-    />
+     <div>
+      <section class="mx-auto max-w-7xl bg-gray-50 px-2 py-10 md:px-0">
+    <div>
+      <div class="mx-auto max-w-2xl lg:text-center">
+        <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+          Blockchain's Role in Pharmaceutical Supply Chain
+        </h2>
+        <p class="mt-4 max-w-xl text-base leading-relaxed text-gray-600 lg:mx-auto">
+        This initiative offers educational materials to highlight the advantages of blockchain technology and managing pharmaceutical supply chains.
+        </p>
       </div>
+      <div class="mx-auto mt-8 max-w-3xl space-y-4 md:mt-16">
+        <div class="cursor-pointer rounded-md border border-gray-400 shadow-lg transition-all duration-200">
+          <button
+            type="button"
+            class="flex w-full items-center justify-between px-4 py-5 sm:p-6"
+          >
+            <Link href='https://www.emerald.com/insight/content/doi/10.1108/MSCRA-10-2022-0023/full/html'>
+            <span class="flex text-lg font-semibold text-black">
+              How do I get started?
+            </span>
+            </Link>
+          </button>
+          <div class="px-4 pb-5 sm:px-6 sm:pb-6">
+            <p class="text-gray-500">
+            To get started, explore our blockchain educational resources for pharmaceutical supply chain benefits.
+            You can access these valuable resources on our website to kickstart your journey into blockchain in pharmaceutical supply chain management.
+            </p>
+          </div>
+        </div>
+        <div class="cursor-pointer rounded-md border border-gray-400 transition-all duration-200">
+          <button
+            type="button"
+            class="flex w-full items-start justify-between px-4 py-5 sm:p-6 md:items-center"
+          >
+
+            <Link href='https://youtu.be/qP37WNQo_hE?si=wHH5UM5vxXerx8i5'>
+            <span class="flex text-start text-lg font-semibold text-black">
+              Access our Blockchain Webinar Series
+            </span>
+            </Link>
+            
+          </button>
+        </div>
+        <div class="cursor-pointer rounded-md border border-gray-400 transition-all duration-200">
+          <button
+            type="button"
+            class="flex w-full items-start justify-between px-4 py-5 sm:p-6 md:items-center"
+          >
+            <Link href='https://www.blockchain-council.org/blockchain/an-ultimate-guide-to-basic-blockchain-technology/'>
+            <span class="flex text-start text-lg font-semibold text-black">
+              Step-by-Step Beginners Guide
+            </span>
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+     </div>
   )
 }
