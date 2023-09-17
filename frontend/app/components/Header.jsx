8e5a1b05a149017400301b57a440c01f47e6671a
Rahul-2704 <rahulruke35@gmail.com>
2023-09-17 10:59:25 +0530

    final

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -1,14 +1,15 @@
 import Link from "next/link"
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useAccount } from 'wagmi';
-
 export default function Header() {
     return (
         <div className="flex items-center justify-between m-2">
             <div>
+                <h1 className="text-2xl font-semibold">PharmaChain</h1>
+            </div>
+            <div>
                 <Link href="/" className="mx-8 text-xl font-bold">Home</Link>
                 <Link href="/dashboard" className="mx-8 text-xl font-bold">Dashboard</Link>
-                <Link href="/contact" className="mx-8 text-xl font-bold">Contact</Link>
             </div>
             <ConnectButton className="mx-8" />
         </div>
